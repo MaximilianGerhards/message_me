069ce70ccae531ce4bb2a692f429460ce0fea821
Maximilian Gerhards <maximilian.gerhards@medgurus.de>
2023-12-13 13:34:31 +0100

    Use messages from the table for the chatroom view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Max Mustermann", password: "Password")
+Message.create(text: "Hi everyone", user: User.last)
+Message.create(text: "Long time no see", user: User.last)
