2b81352d9070720ae6ecb04fcc6f3b25bb8d0477
Maximilian Gerhards <maximilian.gerhards@medgurus.de>
2023-12-14 16:43:34 +0100

    Add authentication functionality and enable flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user, :logged_in?
+    
+    def current_user
+        @current_user ||= User.find(session[:user_id]) unless session[:user_id].nil?
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def require_user
+        if !logged_in?
+            flash[:error] = "You need to be logged in to perform taht action"
+            redirect_to login_path
+        end
+    end
 end
